798c6e8767862afd253ae295e32752542ec6cbf2
wcyuan <wcyuan@ctrip.com>
2014-08-04 19:38:36 +0800

    remove unit test

--- src/main/java/org/mokey/mapping/identifiermanager/Main.java
+++ src/main/java/org/mokey/mapping/identifiermanager/Main.java
@@ -9,23 +9,11 @@ import java.net.URI;
 
 /**
  * Main class.
- *
  */
 public class Main {
-    // Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://localhost:8080/myapp/";
-
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     * @return Grizzly HTTP server.
-     */
     public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in org.mokey.mapping.identifiermanager package
         final ResourceConfig rc = new ResourceConfig().packages("org.mokey.mapping.identifiermanager");
-
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
