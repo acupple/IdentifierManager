f5dc3e7f6db8a7a164d58ab949b36c337e3a8b2c
wcyuan <wcyuan@ctrip.com>
2014-08-04 20:35:12 +0800

    ping

--- src/main/java/org/mokey/mapping/identifiermanager/MyResource.java
+++ src/main/java/org/mokey/mapping/identifiermanager/MyResource.java
@@ -3,13 +3,23 @@ package org.mokey.mapping.identifiermanager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.grizzly.http.server.Request;
 
 @Path("welcome")
 public class MyResource {
+	
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    public Response getIt(@Context Request request) {
+    	StringBuilder sb = new StringBuilder();
+    	sb.append("hello: ").append(System.lineSeparator())
+    		.append(request.getHeader("user-agent")).append(System.lineSeparator())
+    		.append(request.getRemoteAddr()).append(System.lineSeparator())
+    		.append(request.getHeader("accept-language"));
+        return Response.ok(sb.toString()).build();
     }
 }
